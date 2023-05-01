1e275bc854ec43ec4e3c6fd24455a77116866d35
satyasootar <satya.sootar06@gmail.com>
2023-05-02 00:05:49 +0530

    Js pratice

---
+++ JavaScript/index.js
@@ -0,0 +1,10 @@
+//? var _1myAge = '18'
+// ?console.log(_1myAge)
+//? console.log(typeof(_1myAge))
+
+// var myAge = true;
+// console.log(myAge);
+// console.log(typeof(myAge)); 
+
+console.log(3**3);
+console.log(5+7);
